e0c05904635739999b2c5e9c139ec86a5f17f3ee
Carlos969988 <136388389+Carlos969988@users.noreply.github.com>
2023-10-03 00:10:27 -0600

    all

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    // base: 'https://carlos969988.github.io/Carlos969988.github.io/',
+    base: 'https://carlos969988.github.oi/demoShirts.github.io/',
     devOptions: {
       enabled: true,
     },
